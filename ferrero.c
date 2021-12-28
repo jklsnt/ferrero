d06bd61a7f6043cc38b37a5dfe1f620eeb7d15ea
David Freifeld <freifeld.david@gmail.com>
2021-12-27 21:43:02 -0800

    Sort of kind of works

--- ferrero.c
+++ ferrero.c
@@ -1,5 +1,5 @@
 /* Prompt:
-ferrero the woodchuck is tired of being made fun of for not being able to chuck wood. instead of going to the gym to practice throwing things, he's been sitting at home eating this giant tub of popcorn that my mom's work sent us for no apparent reason. he realizes that popcorn, like woodchucks, must be made fun of too. presently, he begins to feel bad and becomes a social justice warrior on a mission to right this wrong in the world. 
+ferrero the woodchuck is tired of being made fun of for not being able to chuck wood. instead of going to the gym to practice throwing things, he's been sitting at home eating this giant tub of popcorn that my mom's work sent us for no apparent reason. he realizes that popcorn, like woodchucks, must be made fun of too. presently, he begins to feel bad and becomes a social justice warrior on a mission to right this wrong in the world.
 
 given a dictionary of N words with average length L letters, each labeled with noun, verb, both, or none, find all words that can be broken down into "[noun][verb]" or "[verb][noun]" so that they might be oppressed
 */
@@ -17,10 +17,23 @@ given a dictionary of N words with average length L letters, each labeled with n
 #define DICTIONARY_LENGTH 121760
 #define NOUN_PATH "/home/quantumish/Wordlist-Nouns-All.txt"
 #define NOUN_LENGTH 90960
-#define VERB_PATH "/home/quantumish/Wordlist-Verbs-All.txt"
+#define VERB_PATH "/home/quantumish/verbs_inf.txt"
 #define VERB_LENGTH 30800
 
-#define BUFFER_SIZE 16*1024
+#define BUFFER_SIZE (16*1024)
+
+#define HASHMAP_LEN (DICTIONARY_LENGTH*200)
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
 
 // djb2 hash
 // http://www.cse.yorku.ca/~oz/hash.html
@@ -34,96 +47,133 @@ size_t hash(char* p, size_t sz) {
     return hash;
 }
 
-int main() {    
-    char* exists = calloc(DICTIONARY_LENGTH/4, 1);
-    char* type = calloc(DICTIONARY_LENGTH/4, 1);
-    char* valid = calloc(DICTIONARY_LENGTH/4, 1);
+int main() {
+    char* exists = calloc(HASHMAP_LEN/8, 1);
+    char* type = calloc(HASHMAP_LEN/8, 1);
+    char* valid = calloc(HASHMAP_LEN/8, 1);
     int fd = open(VERB_PATH, O_RDONLY | O_NONBLOCK);
     posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL);
-    char* buf = malloc(BUFFER_SIZE+1); 
+    char* buf = malloc(BUFFER_SIZE+1);
+    char incomplete_word[45] = {0};
     size_t bytes_read = 0;
+    bool nasty_error = false;
     while((bytes_read = read(fd, buf, BUFFER_SIZE))) {
 	if (bytes_read == 0) break;
 	bool first = true;
-	for(char *p = buf;;) {
-	    char* bound = memchr(p, '\n', (buf + bytes_read) - p);	    
-	    int len = bound - p;	   
+	char* p = buf;
+	while (true) {
+	    // printf("run (%ld bytes left)\n", (buf + bytes_read) - p);
+	    char* bound = memchr(p, '\n', (buf + bytes_read) - p);
+	    int len = bound - p;
+	    if (bound == 0x0) {
+		// printf("Can't find newline!\n");
+		break;
+	    }	    
+	    uint64_t true_index;
 	    if (first) {
-		first = false;
-	        p += len+1;
-		continue;
+		first = false;	        
+		strncat(incomplete_word, p, len+1);
+		size_t word_len = strlen(incomplete_word);
+		/* for (int i = 0; i < word_len; i++) { */
+		/*     printf("%c", incomplete_word[i]); */
+		/* }; printf("\n"); */
+		true_index = hash(incomplete_word, word_len) % HASHMAP_LEN;
+		memset(incomplete_word, 0, 45);
 	    }
-	    if (len <= 0 || bound == 0x0 || p > buf+BUFFER_SIZE) break;
-	    size_t true_index = hash(p, len) % (DICTIONARY_LENGTH*2);	    
+	    else {			        
+		true_index = (uint64_t)hash(p, len) % (uint64_t)HASHMAP_LEN;
+		/* for (int i = 0; i < len; i++) { */
+		/*     printf("%c", p[i]); */
+		/* }; printf(" %zu", true_index); */
+	    }
 	    type[true_index/8] |= 1 << (8-(true_index%8));
+	    /* printf(" %zu "BYTE_TO_BINARY_PATTERN" %d %d "BYTE_TO_BINARY_PATTERN, hash(p, len), BYTE_TO_BINARY(exists[true_index/8]), 8-(true_index%8), true_index%8, BYTE_TO_BINARY(1 << (8-(true_index%8)))); */
 	    exists[true_index/8] |= 1 << (8-(true_index%8));
+	    // printf(" "BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY(exists[true_index/8]));
 	    p += len+1;
 	}
+	if (p != buf + bytes_read && !nasty_error) memcpy(incomplete_word, p, buf+bytes_read-p);
+	nasty_error = false;
     }
-    size_t true_index = hash("run", 3) % (DICTIONARY_LENGTH*2);	    
-    assert((type[true_index/8] & (1 << (8-(true_index%8)))) == (1 << (8-(true_index%8))));
+    uint64_t true_index = hash("lf", 2) % HASHMAP_LEN;
+    assert((exists[true_index/8] & (1 << (8-(true_index%8)))) == 0);
+    true_index = hash("pop", 3) % HASHMAP_LEN;
+    // printf("%zu\n", true_index);
+    assert((exists[true_index/8] & (1 << (8-(true_index%8)))) == (1 << (8-(true_index%8))));
     fd = open(NOUN_PATH, O_RDONLY | O_NONBLOCK);
     posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL);
     while((bytes_read = read(fd, buf, BUFFER_SIZE))) {
 	if (!bytes_read) break;
 	bool first = true;
-	for(char *p = buf;;) {
-	    char* bound = memchr(p, '\n', (buf + bytes_read) - p);	    
-	    int len = bound - p;	   
+	char* p = buf;
+	while (true) {
+	    char* bound = memchr(p, '\n', (buf + bytes_read) - p);
+	    int len = bound - p;
+	    if (bound == 0x0) break;
+	    else if (len <= 0) {
+		nasty_error = true;
+		break;
+	    }
+	    uint64_t true_index;
 	    if (first) {
 		first = false;
-		p += len+1;
-		continue;
+		strncat(incomplete_word, p, len);
+		size_t word_len = strlen(incomplete_word);		
+		true_index = hash(incomplete_word, word_len) % HASHMAP_LEN;
+		memset(incomplete_word, 0, 45);
 	    }
-	    if (len <= 0 || bound == 0x0) break;
-	    size_t true_index = hash(p, len) % (DICTIONARY_LENGTH*2);	    
+	    else true_index = hash(p, len) % HASHMAP_LEN;
 	    exists[true_index/8] |= 1 << (8-(true_index%8));
 	    p += len+1;
 	}
-    }    
-    fd = open(DICTIONARY_PATH, O_RDONLY | O_NONBLOCK);
+	if (p != buf + bytes_read && !nasty_error) memcpy(incomplete_word, p, buf+bytes_read-p);	
+	nasty_error = false;
+    }
+    fd = open(NOUN_PATH, O_RDONLY | O_NONBLOCK);
     posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL);
     while((bytes_read = read(fd, buf, BUFFER_SIZE))) {
 	if (!bytes_read) break;
 	bool first = true;
-	for(char *p = buf;;) {
-	    char* bound = memchr(p, '\n', (buf + bytes_read) - p);	    
-	    int len = bound - p;	   
-	    if (first) {
-		first = false;
-		p += len+1;
-		continue;
+	char *p = buf;
+	while (true) {
+	    char* bound = memchr(p, '\n', (buf + bytes_read) - p);
+	    int len = bound - p;
+	    if (bound == 0x0) break;
+	    else if (len <= 0) {
+		nasty_error = true;
+		break;
 	    }
-	    if (len <= 0 || bound == 0x0) break;
-	    for (size_t i = 1; i < len; i++) {	   
-		size_t pre_index = hash(p, i) % (DICTIONARY_LENGTH*2);
-		size_t post_index = hash(p+i, len-i) % (DICTIONARY_LENGTH*2);		
-		if ((exists[pre_index/8] & (1 << (8-(pre_index%8)))) == (1 << (8-(pre_index%8))) && 
-		    (exists[post_index/8] & (1 << (8-(post_index%8)))) == (1 << (8-(post_index%8))) && 
-		    (type[pre_index/8] & (1 << (8-(pre_index%8)))) != (type[post_index/8] & (1 << (8-(post_index%8))))) {
-		    size_t true_index = hash(p, len) % (DICTIONARY_LENGTH*2);
-		    for (int j = 0; j < len; j++) {
-			printf("%c", *(p+j));		       
+	    if (1 == 1) {
+		for (size_t i = 1; i < len; i++) {
+		    size_t pre_index = hash(p, i) % HASHMAP_LEN;
+		    size_t post_index = hash(p+i, len-i) % HASHMAP_LEN;		    		   
+		    if ((exists[pre_index/8] & (1 << (8-(pre_index%8)))) == (1 << (8-(pre_index%8))) &&
+			(exists[post_index/8] & (1 << (8-(post_index%8)))) == (1 << (8-(post_index%8))) &&
+			(type[pre_index/8] & (1 << (8-(pre_index%8)))) != (type[post_index/8] & (1 << (8-(post_index%8))))) {
+			uint64_t true_index = hash(p, len) % HASHMAP_LEN;
+			for (int j = 0; j < len; j++) {
+			    printf("%c", *(p+j));
+			}
+			printf(" -- ");
+			for (int j = 0; j < i; j++) {
+			    printf("%c", *(p+j));
+			}
+			printf("/");
+			for (int j = 0; j < len-i; j++) {
+			    printf("%c", *(p+i+j));
+			}
+			printf("\n");			
+			valid[true_index/8] |= 1 << (8-(true_index%8));
 		    }
-		    printf(" -- ");
-		    for (int j = 0; j < i; j++) {
-			printf("%c", *(p+j));		       
-		    }
-		    printf("/");
-		    for (int j = 0; j < len-i; j++) {
-			printf("%c", *(p+i+j));		       
-		    }
-		    printf("\n");
-		    valid[true_index/8] |= 1 << (8-(true_index%8));
 		}
-	    }	    
+	    }
 	    p += len+1;
 	}
-    }    
-    true_index = hash("woodchuck", 9) % (DICTIONARY_LENGTH*2);	    
-    assert((exists[true_index/8] & (1 << (8-(true_index%8)))) == (1 << (8-(true_index%8))));
-    assert((type[true_index/8] & (1 << (8-(true_index%8)))) == 0);
-    assert((valid[true_index/8] & (1 << (8-(true_index%8)))) == (1 << (8-(true_index%8))));
+	if (p != buf + bytes_read && !nasty_error) memcpy(incomplete_word, p, buf+bytes_read-p);
+	nasty_error = false;
+    }
+    /* true_index = hash("woodchuck", 9) % HASHMAP_LEN; */
+    /* assert((exists[true_index/8] & (1 << (8-(true_index%8)))) == (1 << (8-(true_index%8)))); */
+    /* assert((type[true_index/8] & (1 << (8-(true_index%8)))) == 0); */
+    /* assert((valid[true_index/8] & (1 << (8-(true_index%8)))) == (1 << (8-(true_index%8)))); */
 }
-
-
